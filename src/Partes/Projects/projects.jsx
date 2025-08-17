3075d920121cec54fb6ab8b871e03035e52869fd
Hansen Gaspar <hansengaspar01@gmail.com>
2025-08-17 12:53:21 -0300

    Version 1.0

--- src/Partes/Projects/projects.jsx
+++ src/Partes/Projects/projects.jsx
@@ -8,7 +8,6 @@ import UltraNoti from '../../json/Proyectos/UltraNoti.json';
 import Fakebook from '../../json/Proyectos/Fakebook.json';
 import BetterSID from '../../json/Proyectos/BetterSID.json';
 import Scrapers from '../../json/Proyectos/Scrapers.json';
-import LoginTest from '../../json/Proyectos/LoginTest.json';
 import BuscaProductores from '../../json/Proyectos/BuscaProductores.json';
 
 function projects() {
@@ -16,15 +15,15 @@ function projects() {
         <div className="projects mx-4 App">
             <H2Tittle>Proyectos</H2Tittle>
             <div className='flex flex-col m-2 gap-2'>
+                <ProjectBox data={BuscaProductores} />
+                <ProjectBox data={BetterSID} />
                 <ProjectBox data={Fakebook} />
                 <ProjectBox data={UltraNoti} />
-                <ProjectBox data={BetterSID} />
                 <ProjectBox data={Scrapers} />
-                <ProjectBox data={LoginTest} />
-                <ProjectBox data={BuscaProductores} />
+
             </div>
         </div>
     )
-}  
+}
 
 export default projects;
